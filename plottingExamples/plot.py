eb2bad2b523d4da18e1066aab5c6178f895993db
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2022-08-25 19:22:55 -0500

    examples

--- plottingExamples/plot.py
+++ plottingExamples/plot.py
@@ -8,9 +8,30 @@ tf = ROOT.TFile('histOut.root')
 tf.cd("plots")
 tf.ls()
 
-npdf=100;
+npdf=100
 th={}
 xsec=[]
+signal_peak_frac=[]
+
+# ---
+def get_mean_PDFerror(arr):
+    #print(arr)
+    nparr = np.array(arr)
+    nparr_mean = np.array(nparr.size * [np.mean(nparr)])
+
+    # mean yield
+    mean=np.mean(nparr)
+
+    # variation in the yield for PDF error
+    # https://arxiv.org/abs/1510.03865
+    err=np.sqrt(np.mean((nparr-nparr_mean)**2))
+    if nparr.size>0:
+        err = err * nparr.size / (nparr.size - 1)
+
+    return mean, err
+
+# ---
+mpeak=5000
 
 for i in range(0, npdf+1):
     th[i] = tf.Get("plots/mass"+str(i))
@@ -25,22 +46,21 @@ for i in range(0, npdf+1):
         th[i].Draw("hist")
     else:
         th[i].Draw("sames,hist")
+        signal_peak_frac.append(th[i].Integral(th[i].FindBin(mpeak*0.85),th[i].FindBin(mpeak*1.15)))
 
-print(xsec)
-nparr_xsec = np.array(xsec)
-print(nparr_xsec)
+# ---
+# xsec(yield) array
+#
+print("yields mean and fractional error")
 
-nparr_xsec_mean = np.array(nparr_xsec.size * [np.mean(nparr_xsec)])
-print(nparr_xsec_mean)
+yield_mean, yield_err = get_mean_PDFerror(xsec)
+print(yield_mean, yield_err/yield_mean)
 
-# mean yield
-yield_mean=np.mean(nparr_xsec)
-print(yield_mean)
+# ---
+# mean signal peak fraction
+#
 
-# variation in the yield for PDF error
-# https://arxiv.org/abs/1510.03865
-yield_rms=np.sqrt(np.mean((nparr_xsec-nparr_xsec_mean)**2))
-if nparr_xsec.size>0:
-    yield_rms = yield_rms * nparr_xsec.size / (nparr_xsec.size - 1)
-print(yield_rms / yield_mean)
+print("signal_peak_frac (+-15% of the resonance mass) mean and fractional error")
 
+signal_peak_frac_mean, signal_peak_frac_err = get_mean_PDFerror(signal_peak_frac)
+print(signal_peak_frac_mean, signal_peak_frac_err/signal_peak_frac_mean)
